e55d3069cfcf75e8c8f329176526d568aa218d8e
Alfiya Simran <alfiyasimran05@gmail.com>
2025-08-02 20:06:09 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from "./Navbar";
 import LoginPage from './LoginPage';
 import Dashboard from './Dashboard';
 import SignupPage from "./SignupPage";
 import Leaderboard from "./Leaderboard";
-import Navbar from "./Navbar";
 function App() {
   return (
     <Router>
