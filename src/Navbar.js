0df47067fa016e217868b2874c2bf912e304f946
Alfiya Simran <alfiyasimran05@gmail.com>
2025-08-02 20:03:14 +0530

    Update Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,20 +1,13 @@
 // src/components/Navbar.js
-import React from "react";
-import { Link, useLocation } from "react-router-dom";
-import "./Navbar.css";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css'; // optional styling
 
 const Navbar = () => {
-  const location = useLocation();
-
-  // Show navbar only on /dashboard or /leaderboard
-  const showNav = ["/dashboard", "/leaderboard"].includes(location.pathname);
-
-  if (!showNav) return null;
-
   return (
     <nav className="navbar">
-      <div className="navbar-logo">Intern Portal</div>
-      <div className="navbar-links">
+      <h3>Intern Portal</h3>
+      <div className="nav-links">
         <Link to="/dashboard">Dashboard</Link>
         <Link to="/leaderboard">Leaderboard</Link>
       </div>
