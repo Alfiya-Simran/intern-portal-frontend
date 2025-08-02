74ae67ea41cf5a060025b7b5f4677efcb2a9c4ba
Alfiya Simran <alfiyasimran05@gmail.com>
2025-08-02 20:10:44 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Navbar from "./Navbar";
 import LoginPage from './LoginPage';
 import Dashboard from './Dashboard';
 import SignupPage from "./SignupPage";
 import Leaderboard from "./Leaderboard";
+
+function AppWrapper() {
+  const location = useLocation();
+  const hideNavbar = location.pathname === '/' || location.pathname === '/signup';
+
+  return (
+    <>
+      {!hideNavbar && <Navbar />}
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/leaderboard" element={<Leaderboard />} />
+      </Routes>
+    </>
+  );
+}
+
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="/leaderboard" element={<Leaderboard />} />
-      </Routes>
+      <AppWrapper />
     </Router>
   );
 }
