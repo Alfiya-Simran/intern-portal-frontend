bf29e46e10c4da0cf236088121a36275ceb841c1
Alfiya Simran <alfiyasimran05@gmail.com>
2025-08-02 19:49:49 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './LoginPage';
 import Dashboard from './Dashboard';
 import SignupPage from "./SignupPage";
+import Leaderboard fron "./Leaderboard";
 function App() {
   return (
     <Router>
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<LoginPage />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/signup" element={<SignupPage />} />
+        <Route path="/leaderboard" element={<Leaderboard/>} />
       </Routes>
     </Router>
   );
