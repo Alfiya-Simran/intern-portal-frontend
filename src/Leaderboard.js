9d1470d0fbfc938c8f882866aee0b36069567d15
Alfiya Simran <alfiyasimran05@gmail.com>
2025-08-02 20:11:24 +0530

    Update Leaderboard.js

--- src/Leaderboard.js
+++ src/Leaderboard.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import Navbar from './Navbar';
 const API_BASE = "https://intern-portal-backend-wuk4.onrender.com";
 
 const Leaderboard = () => {
